c1c09123a5bd99033e0e605ca494c16d9e879d18
Aleksandr-front-end-developer <oleksandr.danyliuk.88@gmail.com>
2025-12-03 20:57:17 +0200

    fix breadcrumb for template contact

--- wp-content/themes/tromso/templates/contact.php
+++ wp-content/themes/tromso/templates/contact.php
@@ -12,30 +12,7 @@ $post_id = get_the_ID();
 ?>
 <main id="main" class="main">
 
-	<section class="breadcrumbs">
-		<div class="tour-hero__container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-			<div itemscope itemtype="https://schema.org/BreadcrumbList" class="breadcrumbs__container">
-				<span itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-					<a class="transition-all duration-300" itemprop="item" href="<?php echo home_url(); ?>">
-						<span itemprop="name"><?php echo get_bloginfo('name'); ?></span>
-						<meta itemprop="position" content="1" />
-					</a>
-				</span>
-				<?php if ($post && !is_home() && !is_archive()) : ?>
-					<span itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-						<span class="divider">
-							<svg class="svg" width="24" height="24">
-								<use xlink:href="<?php echo THEME_URI . '/assets/img/icons/icons.svg#arrow-right-24'; ?>"></use>
-							</svg>
-						</span>
-						<span itemprop="name"><?php echo get_the_title($post); ?></span>
-						<meta itemprop="position" content="2" />
-					</span>
-				<?php endif; ?>
-			</div>
-		</div>
 
-	</section>
 
 	<?php
 	$title = carbon_get_post_meta($post_id, 'cnt_section_1_title');
