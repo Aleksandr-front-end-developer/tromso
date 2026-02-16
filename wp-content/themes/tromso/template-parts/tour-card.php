1c560be329db4a1ab5b2c12b14c176ad57dd1654
Игорь Чулков <chimgan-75@yandex.ru>
2026-02-16 11:28:45 +0500

    Замена названий кнопок

--- wp-content/themes/tromso/template-parts/tour-card.php
+++ wp-content/themes/tromso/template-parts/tour-card.php
@@ -104,7 +104,7 @@ if (!is_null($tour))
                 ?>
           			<a href="<?php echo $url; ?>" target="_blank"
           				class="px-6 py-3 rounded-lg font-semibold transition-all duration-300 transform hover:scale-105 active:scale-95 bg-aurora-green text-white hover:bg-green-600 shadow-lg hover:shadow-xl px-6 py-2 card__button">
-          				BOOK NOW
+          				CHECK AVAILABILITY
           			</a>
                 <?php } } ?>
           		</div>
